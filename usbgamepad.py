f6d5beb905457c7a7bc6c5378a8e0db1f21e8478
sam blenny <68084116+samblenny@users.noreply.github.com>
2024-07-27 06:44:59 +0000

    find all USB devices

--- usbgamepad.py
+++ usbgamepad.py
@@ -3,6 +3,14 @@
 #
 # usb-gamepad
 #
+# Interesting vendor:product IDs:
+#  045e:028e  XInput Gamepad (*many* gamepads and adapters match this)
+#  2dc8:3107  8BitDo 8BitDo USB Wireless Adapter 2 (idle, no connection)
+#
+# The 8BitDo adapter switches vendor and product IDs when it has a connection
+# to a Bluetooth gamepad. When idle, it presents as 2dc8:3107. When connected,
+# it presents as 045e:028e (unless you set it to some other mode?).
+#
 import usb
 
 print("\n\n## Compare dir(usb) between Bookworm and CircuitPython\n")
@@ -16,14 +24,21 @@ for n in names:
     print("  %s" % n)
 
 print("\n\n## Dump info on connected USB devices\n")
-devices = usb.core.find()
+devices = list(usb.core.find(find_all=1))
+skip = ["backend", "manufacturer", "product", "serial_number", "parent"]
 for d in devices:
-    print("====================================================")
-    names = [n for n in dir(d) if not n.startswith("_")]
+    print("\n" + "=" * 60)
+    print(f"=== {d.idVendor:04x}:{d.idProduct:04x} ======" + ("=" * 40))
+    print("=" * 60)
+    names = [n for n in dir(d) if not (n.startswith("_") or (n in skip))]
     for n in names:
-        val = getattr(d, n)
+        val = ""
+        try:
+            val = getattr(d, n)
+        except ValueError as e:
+            val = e
         T = type(val)
-        if T in [int, list]:
+        if T in [int, list, tuple]:
             print("[%s]:" % n, val)
         elif T == usb.core.Device:
             print("[%s]:\n" % n, val)
