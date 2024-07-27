6fcb4ff867d0b4b6020f7aeac26a64dd833b9081
sam blenny <68084116+samblenny@users.noreply.github.com>
2024-07-27 23:57:48 +0000

    narrow scan using vid:pid

--- usbgamepad.py
+++ usbgamepad.py
@@ -1,48 +1,34 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: Copyright 2024 Sam Blenny
-#
-# usb-gamepad
-#
-# Interesting vendor:product IDs:
-#  045e:028e  XInput Gamepad (*many* gamepads and adapters match this)
-#  2dc8:3107  8BitDo 8BitDo USB Wireless Adapter 2 (idle, no connection)
-#
-# The 8BitDo adapter switches vendor and product IDs when it has a connection
-# to a Bluetooth gamepad. When idle, it presents as 2dc8:3107. When connected,
-# it presents as 045e:028e (unless you set it to some other mode?).
-#
-import usb
+"""
+usb-gamepad
 
-print("\n\n## Compare dir(usb) between Bookworm and CircuitPython\n")
-print("dir(usb.core) [circuitpython usb]:")
-for n in sorted(["find", "Device", "USBError", "USBTimeoutError"]):
-    print("  %s" % n)
-print()
-print("dir(usb.core) [bookworm python3-usb]:")
-names = sorted([n for n in dir(usb.core) if not n.startswith("_")])
-for n in names:
-    print("  %s" % n)
+Interesting vendor:product IDs:
+  045e:028e  XInput Gamepad (*many* gamepads and adapters match this)
+  2dc8:3107  8BitDo 8BitDo USB Wireless Adapter 2 (idle, no connection)
 
-print("\n\n## Dump info on connected USB devices\n")
-devices = list(usb.core.find(find_all=1))
-skip = ["backend", "manufacturer", "product", "serial_number", "parent"]
-for d in devices:
-    print("\n" + "=" * 60)
-    print(f"=== {d.idVendor:04x}:{d.idProduct:04x} ======" + ("=" * 40))
-    print("=" * 60)
-    names = [n for n in dir(d) if not (n.startswith("_") or (n in skip))]
-    for n in names:
-        val = ""
-        try:
-            val = getattr(d, n)
-        except ValueError as e:
-            val = e
-        T = type(val)
-        if T in [int, list, tuple]:
-            print("[%s]:" % n, val)
-        elif T == usb.core.Device:
-            print("[%s]:\n" % n, val)
-        elif callable(val):
-            print("[%s]: <callable>" % n)
-        else:
-            print("[%s, %s]:" % (n, T), val)
+The 8BitDo adapter switches vendor and product IDs when it has a connection
+to a Bluetooth gamepad. When idle, it presents as 2dc8:3107. When connected,
+it presents as 045e:028e (unless you set it to some other mode?).
+
+names available in the CircuitPython version of usb.core:
+ find Device USBError USBTimeoutError
+The full PyUSB version has more classes and methods, but those extra things
+won't transfer over to CircuitPython, so I'm ignoring them.
+"""
+from usb import core
+
+
+# This calls usb.core.find() in a way that only works correctly if you have
+# just one connected gamepad (for more, it needs a find_all=1 argument)
+print("Looking for USB gamepads...")
+idle_adapter = core.find(idVendor=0x2dc8, idProduct=0x3107)
+gamepad = core.find(idVendor=0x045e, idProduct=0x028e)
+if idle_adapter:
+    print("\nFound an 8BitDo Adapter in idle mode (2dc8:3107)\n")
+    print(idle_adapter)
+if gamepad:
+    print("\nFound an XInput gamepad (045e:028e)")
+    print(gamepad)
+else:
+    print("\nIt seems no gamepads are currently connected")
